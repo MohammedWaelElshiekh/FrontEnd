ea072758e24fdc1d5c5ad2afc86fc70c4b1c6183
Mohammed Wael Elshiekh <mohammedwaelelshiekhmw@gmail.com>
2024-07-14 17:53:12 +0300

    added the functionality of refreshing the images and showing the backend errors

--- js/app.js
+++ js/app.js
@@ -2,16 +2,21 @@ const imagesContainer = document.getElementById("gallery");
 const spinner = document.getElementById("spinner");
 const selectFile = document.getElementById("image");
 var selectedImage; // a global variable to save the selected image frim the gallery
-(async () => {
+var res;
+const refreshGallery = async () => {
+  imagesContainer.innerHTML = "";
+  selectFile.removeAttribute("disabled");
+
   // fetch Images Library List
-  const data = await fetch("http://localhost:3001/library", {
+
+  const data = await fetch("http://localhost:4000/library", {
     method: "GET",
   });
   const library = await data.json();
   // fetch Images from the library using the provided list
   let images = await Promise.all(
     library.map((image) =>
-      fetch(`http://localhost:3001/public/library/${image}`)
+      fetch(`http://localhost:4000/public/library/${image}`)
     )
   );
   // extracts the images to be actual elements using the blobs
@@ -26,32 +31,37 @@ var selectedImage; // a global variable to save the selected image frim the gall
       if (imageElement.classList.contains("selected")) {
         imageElement.classList.remove("selected");
         selectFile.removeAttribute("disabled");
+        selectedImage = null;
       } else {
         for (i = 0; i < imagesContainer.children.length; i++) {
           imagesContainer.children.item(i).classList.remove("selected");
         }
         imageElement.classList.add("selected");
         selectFile.setAttribute("disabled", "true");
+        selectFile.value = null;
         selectedImage = imageElement.imgName;
       }
     };
     imagesContainer.appendChild(imageElement);
   });
   spinner.style.display = "none";
-})();
-
+};
+refreshGallery(); // calls it once to get the images
 // Upload Image to the server when a submit button is clicked and then fetches the created image and shows it with other images on the gallery
 document.getElementById("submitButton").onclick = async () => {
   const formData = new FormData(document.getElementById("uploadForm"));
   formData.append("selectedImage", selectedImage);
-  fetch("http://localhost:3001/ResizeImage", {
+  fetch("http://localhost:4000/ResizeImage", {
     method: "POST",
     body: formData,
   }).then(async (response) => {
-    const blob = await response.blob();
-    const imageUrl = URL.createObjectURL(blob);
-    const image = document.createElement("img");
-    image.src = imageUrl;
-    document.getElementById("preview").appendChild(image);
+    const data = await response.json();
+    if (response.ok) {
+      document.getElementById("previewImage").setAttribute("src", data.url);
+      document.getElementById("url").innerText = data.url;
+      refreshGallery();
+    } else {
+      alert("Error: " + data.err + " Code:" + response.status);
+    }
   });
 };
